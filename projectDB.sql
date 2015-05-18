17fbb6d89b6a2ba167c77c2b648df86346771864
DATTV <dattvgc00735@fpt.edu.vn>
2015-05-18 15:23:16 +0700

    Update Database

--- projectDB.sql
+++ projectDB.sql
@@ -1,7 +1,8 @@
-create database EasyTicket
+﻿create database EasyTicket
 use EasyTicket
 go
 
+-- Start Data Not Change -- 
 create table [user](
 	id int not null identity primary key,
 	username varchar(30) not null,
@@ -17,51 +18,13 @@ create table [user](
 	[password] varchar(30) not null
 )
 
-create table [Event](
-	id int not null identity primary key,
-	[type] varchar(10),
-	startdate datetime not null,
-	enddate datetime not null,
-	city varchar(100),
-	[address] varchar(100),
-	caption varchar(100) not null,
-	content text not null,
-	userid int foreign key references [User](id),
-	created timestamp not null
-)
-
-create table [Ticket](
-	id int not null identity primary key,
-	useid int foreign key references [User](id),
-	eventid int foreign key references [Event](id),
-	line varchar(50),
-	created timestamp not null,
-	status varchar(10)
-)
-
-create table [StaticInfo](
-	id int not null identity primary key,
-	[type] varchar(50),
-	caption varchar(200) not null,
-	content text not null,
-	userid int foreign key references [User](id),
-	created timestamp not null
-)
-
-create table [Seat](
-	id int not null identity primary key,
-	eventid int foreign key references [Event](id),
-	area varchar(50),
-	line int,
-	[rank] int
-)
-
-create table [Price](
-	id int not null identity primary key,
-	eventid int foreign key references [Event](id),
-	seatid int foreign key references [Seat](id),
-	lineNumbet int,
-	[price] float
+create table Adminmaster(
+id int identity primary key,
+username nvarchar(50) not null,
+[password] nvarchar(50) not null,
+fullname nvarchar(100),
+email nvarchar(100),
+staffid nvarchar(20) not null
 )
 
 create table [ContactUs](
@@ -72,18 +35,68 @@ create table [ContactUs](
 	created timestamp not null
 )
 
-select * from [user]
-select * from [Event]
-select * from [Ticket]
-select * from [ContactUs]
-select * from [Price]
-select * from [Seat]
-select * from [StaticInfo]
+-- end ---
 
-drop table [user]
-drop table [Event]
-drop table [ContactUs]
-drop table [Price]
-drop table [Seat]
-drop table [StaticInfo]
-drop table [Ticket]
+
+-- main data --
+
+create table location(
+id int identity primary key,
+city nvarchar(30) not null,
+[address] nvarchar(100)
+)  -- dia diem dien ra ---
+
+create table [Type](
+id int identity primary key,
+typeName nvarchar(50) not null
+) -- the loai event : music, movie, sport... ---
+
+create table [Event](
+	id int not null identity primary key,
+	typeid int foreign key references [Type](id),
+	startdate datetime not null,
+	enddate datetime not null,
+	eventname varchar(100) not null,
+	content text not null,
+	location int foreign key references location(id),
+	created timestamp not null
+) --- thong tin chi tiet su kien --- 
+
+create table Arena(
+	id int not null identity primary key,
+	eventid int foreign key references [Event](id),
+	area varchar(50) not null, -- vip hay thuong ---
+	quantity int not null --co bao nhieu ve cho khu vuc do--
+) -- khu vuc cho moi event --
+
+
+create table [Price](
+	id int not null identity primary key,
+	arenaId int foreign key references Arena(id),
+	[price] float
+)-- gia ve cho moi id khu vuc --
+
+
+create table [Ticket](
+	id int not null identity primary key,
+	useid int foreign key references [User](id),
+	eventid int foreign key references [Event](id),
+	Arena int foreign key references Arena(id),
+	created timestamp not null,
+	[status] varchar(10)
+) --- thong tin ve da ban---
+
+create table [order](
+id int identity primary key,
+created timestamp,
+usedOrder int foreign key references [user](id),
+totalPay float not null,
+) --- hoa don ---
+
+create table orderDetail(
+id int identity primary key,
+orderId int foreign key references [order](id), -- ma order 
+eventId int foreign key references [Event](id), -- event order
+Arena int foreign key references [Event](id), -- ma khu vuc
+amount float not null -- so tien
+) --
