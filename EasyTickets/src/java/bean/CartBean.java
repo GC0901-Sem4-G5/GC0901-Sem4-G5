2811160f6fee28582e819bfbe8e5dfd40c597d20
dattv <dattvgc00735@fpt.edu.vn>
2015-07-17 16:12:29 +0700

    update checkout

--- EasyTickets/src/java/bean/CartBean.java
+++ EasyTickets/src/java/bean/CartBean.java
@@ -7,6 +7,7 @@ package bean;
 
 import entity.cart;
 import entity.event;
+import entity.user;
 import java.awt.event.ActionEvent;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -20,6 +21,7 @@ import javax.faces.context.ExternalContext;
 import javax.faces.context.FacesContext;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
+import model.AccountModel;
 import model.CartModel;
 import model.getEventDetail;
 
@@ -38,6 +40,15 @@ public class CartBean {
     private int idEvent;
     private int idArena;
     private event e = new event();
+    private int quantityup;
+
+    public int getQuantityup() {
+        return quantityup;
+    }
+
+    public void setQuantityup(int quantityup) {
+        this.quantityup = quantityup;
+    }
 
     public int getIdEvent() {
         return idEvent;
@@ -87,32 +98,98 @@ public class CartBean {
         this.Quantity = Quantity;
     }
 
+    public int getIndex(String proName) {
+        for (int i = 0; i < list.size(); i++) {
+            String auction = list.get(i).getEventName();
+            if (proName.equals(auction)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public int getexist(String proName, double price) {
+        for (int i = 0; i < list.size(); i++) {
+            String auction = list.get(i).getEventName();
+            double price2 = list.get(i).getPrice();
+            if (proName.equals(auction) && price == price2) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public void updateQuantity(String eventName) {
+        int i = getIndex(eventName);
+        list.get(i).setQuantity(quantityup);
+        quantityup = 1;
+    }
+
+    public void remote(String eventName) {
+        int i = getIndex(eventName);
+        list.remove(i);
+    }
+
     public void AddToCart() {
+
         FacesContext context = FacesContext.getCurrentInstance();
         HttpServletRequest request = (HttpServletRequest) context.getExternalContext().getRequest();
         HttpSession appsession = request.getSession(true);
-        String attribute = (String) appsession.getAttribute("eventid");
-        getEventDetail ed = new getEventDetail();
-        idEvent = Integer.parseInt(attribute);
-        System.out.println(idEvent);
-        System.out.println(Quantity);
-        System.out.println(idArena);
-        cart c = new cart();
-        c.setQuantity(Quantity);
-        c.setEventName(ed.getNamebyID(idEvent));
-        c.setArena(ed.getArenabyID(idArena));
-        c.setPrice(ed.getPricebyID(idArena));
-        list.add(c);
-        shop.ins(c);
-        appsession.setAttribute("cartlist", shop.getIt());
-        ExternalContext extContext = context.getExternalContext();
-        String url = extContext.encodeActionURL(context.getApplication().
-                getViewHandler().getActionURL(context, "/cart.xhtml"));
-        try {
-            extContext.redirect(url);
-        } catch (IOException ex) {
-            Logger.getLogger(AccountBean.class.getName()).log(Level.SEVERE, null, ex);
+        if (null == appsession.getAttribute("username")) {
+            ExternalContext extContext = context.getExternalContext();
+            String url = extContext.encodeActionURL(context.getApplication().
+                    getViewHandler().getActionURL(context, "/login.xhtml"));
+            try {
+                extContext.redirect(url);
+            } catch (IOException ex) {
+                Logger.getLogger(AccountBean.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        } else {
+            String attribute = (String) appsession.getAttribute("eventid");
+            getEventDetail ed = new getEventDetail();
+            idEvent = Integer.parseInt(attribute);
+            System.out.println(idEvent);
+            System.out.println(Quantity);
+            System.out.println(idArena);
+            cart c = new cart();
+            c.setQuantity(Quantity);
+            c.setEventName(ed.getNamebyID(idEvent));
+            c.setArena(ed.getArenabyID(idArena));
+            c.setPrice(ed.getPricebyID(idArena));
+            if (getexist(c.getEventName(), c.getPrice()) < 0) {
+                list.add(c);
+                Quantity = 1;
+            }
+            ExternalContext extContext = context.getExternalContext();
+            String url = extContext.encodeActionURL(context.getApplication().
+                    getViewHandler().getActionURL(context, "/cart.xhtml"));
+            try {
+                extContext.redirect(url);
+            } catch (IOException ex) {
+                Logger.getLogger(AccountBean.class.getName()).log(Level.SEVERE, null, ex);
+            }
         }
+    }
+
+    public double getTotal() {
+        double total = 0;
+        for (int i = 0; i < list.size(); i++) {
+            total = total + list.get(i).getPrice() * list.get(i).getQuantity();
+        }
+        return total;
+    }
+
+    public user getuser() {
+        AccountModel acc = new AccountModel();
+        FacesContext context = FacesContext.getCurrentInstance();
+        HttpServletRequest request = (HttpServletRequest) context.getExternalContext().getRequest();
+        HttpSession appsession = request.getSession(true);
+
+        return acc.getInfobyusername((String) appsession.getAttribute("username"));
+    }
+
+    public String redirect() {
+        return "checkout";
     }
 
 }
