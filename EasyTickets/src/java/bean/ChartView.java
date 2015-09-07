ef1160053669a1b65b40f6bca6d28dcf6fcfdcac
HaNguyenHoang <nguyenhoangha_championship1210@yahoo.com>
2015-09-07 13:01:23 +0700

    Statistics

--- EasyTickets/src/java/bean/ChartView.java
+++ EasyTickets/src/java/bean/ChartView.java
@@ -38,32 +38,22 @@ public class ChartView implements Serializable {
         LineChartSeries series1 = new LineChartSeries();
         series1.setLabel("Series 1");
  
-        series1.set("2014-01-01", 51);
-        series1.set("2014-01-06", 22);
-        series1.set("2014-01-12", 65);
-        series1.set("2014-01-18", 74);
-        series1.set("2014-01-24", 24);
-        series1.set("2014-01-30", 51);
+        series1.set("2015-09-02", 51);
+        series1.set("2015-09-08", 22);
+        series1.set("2015-09-18", 65);
+        series1.set("2015-09-29", 74);
+        series1.set("2015-10-06", 24);
+        series1.set("2015-10-07", 51);
  
-        LineChartSeries series2 = new LineChartSeries();
-        series2.setLabel("Series 2");
- 
-        series2.set("2014-01-01", 32);
-        series2.set("2014-01-06", 73);
-        series2.set("2014-01-12", 24);
-        series2.set("2014-01-18", 12);
-        series2.set("2014-01-24", 74);
-        series2.set("2014-01-30", 62);
  
         dateModel.addSeries(series1);
-        dateModel.addSeries(series2);
          
         dateModel.setTitle("Ticket Sold");
         dateModel.setZoom(true);
         dateModel.getAxis(AxisType.Y).setLabel("Values");
         DateAxis axis = new DateAxis("Dates");
         axis.setTickAngle(-50);
-        axis.setMax("2014-02-01");
+        axis.setMax("2015-12-01");
         axis.setTickFormat("%b %#d, %y");
          
         dateModel.getAxes().put(AxisType.X, axis);
